1147a1b9c373161a70b8b9eeef452a98e594e90c
omer imanov <omer.imanov@gmail.com>
2022-06-30 15:58:05 +0400

    feat:added practise task

---
+++ ASP.NET.Practice/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET.Practice.Controllers
+{
+    public class BlogController:Controller
+    {
+
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ASP.NET.Practice/DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET.Practice.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+    }
+}
